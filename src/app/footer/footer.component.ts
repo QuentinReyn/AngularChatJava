c26eddfddf1b6a5db857f781f0b76c6cc6beb64b
QuentinReynaud <quentin.reynaud99@gmail.com>
2020-10-27 09:34:03 +0100

    [update]footer content changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(){ }
 
-  ngOnInit(): void {
+  appVersion = "";
+  lastBuildDate = new Date();
+
+  ngOnInit() {
+    this.appVersion = "0.01";
   }
-
 }
