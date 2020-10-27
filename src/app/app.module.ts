ed63e41a12c5c4bdaa1e6c3dd333ba001252b9ed
QuentinReynaud <quentin.reynaud99@gmail.com>
2020-10-27 09:07:34 +0100

    [add]sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,10 +40,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
