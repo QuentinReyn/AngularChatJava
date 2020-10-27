e69045faa724f1575458b24494625b130f73e7fe
QuentinReynaud <quentin.reynaud99@gmail.com>
2020-10-27 16:51:36 +0100

    mock users & chatrooms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,204 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+
+const users = [
+  {
+    account: { id: 0, username: 'Zero' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 1, username: 'Zero1' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 2, username: 'Zero2' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 3, username: 'Zero3' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 4, username: 'Zero4' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 5, username: 'Zero5' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 6, username: 'Zero6' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 7, username: 'Zero7' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 8, username: 'Zero8' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+  {
+    account: { id: 9, username: 'Zero9' },
+    currentStatus: 'active',
+    server: 'test',
+  },
+];
+
+const chatrooms = [
+  {
+    id: 0,
+    name: 'aa',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 11,
+    name: 'zae',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 12,
+    name: 'azezaea',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 13,
+    name: 'ezaeae',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 14,
+    name: 'zaere',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 15,
+    name: 'fsdfsf',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 16,
+    name: 'rezrc',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 17,
+    name: 'yetet',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 18,
+    name: 'zazdae',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 19,
+    name: 'vdaeze',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+  {
+    id: 20,
+    name: 'gzerr',
+    owner: {
+      account: { id: 0, username: 'Zero' },
+      currentStatus: 'active',
+      server: 'test',
+    },
+    messages: [
+      { id: 0, content: 'test0' },
+      { id: 1, content: 'test1' },
+    ],
+  },
+];
 
 @Component({
   selector: 'app-home',
@@ -8,121 +208,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   constructor() {}
   title = 'Test Dashboard';
-  users = [
-    { id: 0, username: 'Zero' },
-    { id: 11, username: 'Mr. Nice' },
-    { id: 12, username: 'Narco' },
-    { id: 13, username: 'Bombasto' },
-    { id: 14, username: 'Celeritas' },
-    { id: 15, username: 'Magneta' },
-    { id: 16, username: 'RubberMan' },
-    { id: 17, username: 'Dynama' },
-    { id: 18, username: 'Dr IQ' },
-    { id: 19, username: 'Magma' },
-    { id: 20, username: 'Tornado' },
-  ];
 
-  chatrooms = [
-    {
-      id: 0,
-      name: 'aa',
-      ownerId: 11,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 11,
-      name: 'zae',
-      ownerId: 12,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 12,
-      name: 'azezaea',
-      ownerId: 13,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 13,
-      name: 'ezaeae',
-      ownerId: 14,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 14,
-      name: 'zaere',
-      ownerId: 15,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 15,
-      name: 'fsdfsf',
-      ownerId: 16,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 16,
-      name: 'rezrc',
-      ownerId: 17,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 17,
-      name: 'yetet',
-      ownerId: 18,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 18,
-      name: 'zazdae',
-      ownerId: 19,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 19,
-      name: 'vdaeze',
-      ownerId: 20,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-    {
-      id: 20,
-      name: 'gzerr',
-      ownerId: 0,
-      messages: [
-        { id: 0, content: 'test0' },
-        { id: 1, content: 'test1' },
-      ],
-    },
-  ];
+  displayedColumns: string[] = ['server', 'accountId', 'accountUsername', 'status'];
+  dataSource = users;
+  displayedColumns1: string[] = ['server', 'name', 'ownerName'];
+  dataSource1 = chatrooms;
 
   ngOnInit(): void {}
 }
