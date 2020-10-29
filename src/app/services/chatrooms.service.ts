79c9a7c9c4b4a144cfa2ab22dd55b929614641bf
QuentinReynaud <quentin.reynaud99@gmail.com>
2020-10-29 09:45:44 +0100

    [add] new api call to get Observable<Chatroom[]>

--- src/app/services/chatrooms.service.ts
+++ src/app/services/chatrooms.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Chatroom } from '../models/chatroom.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,11 @@ export class ChatroomsService {
   serverUrl1 = 'http://127.0.0.1:2345';
 
   // tslint:disable-next-line: typedef
-  getChatrooms(): Observable<string[]> {
+  getChatroomNames(): Observable<string[]> {
     return this.http.get<string[]>(this.serverUrl1 + '/chatrooms');
+  }
+
+  getChatrooms(): Observable<Chatroom[]> {
+    return this.http.get<Chatroom[]>(this.serverUrl1 + '/chatrooms/list');
   }
 }
